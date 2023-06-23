83c5a5ba9948c494d67b2804f654b97084553b38
ZemZ12 <123273944+ZemZ12@users.noreply.github.com>
2023-06-23 17:43:36 -0400

    Add files via upload
    added recursive function to search

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,7 +2,24 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
+	if(low <= high){
+
+		 int mid = low + (high - low) / 2;
+
+    
+    if (numbers[mid] == value)
+        return mid;
+    
+   
+    if (numbers[mid] > value)
+        return search(numbers, low, mid - 1, value);
+    
+    
+    return search(numbers, mid + 1, high, value);
+	}
+	
 	return -1;
+	
 }
 
 void printArray(int numbers[], int sz)
